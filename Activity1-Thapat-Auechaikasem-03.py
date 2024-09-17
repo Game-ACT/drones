a18549e78edd3faf65d2408d745e4e89b834ba45
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-09-17 15:27:30 +0700

    Added Files

---
+++ Activity1-Thapat-Auechaikasem-03.py
@@ -0,0 +1,6 @@
+# Async
+print("Hello my name is Thapat Auechaikasem")
+print("My classroom is M.2/2")
+print("My ID number is 28768")
+print("I'm am 13 years old")
+print("I'm in Innovative Intelligence")
